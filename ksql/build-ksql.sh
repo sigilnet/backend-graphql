0fd5d16b993cfb7cd6c3d4b519c07e3676a59f51
nguquen <thienlduc@gmail.com>
2022-08-06 00:09:48 +0700

    [ksql] improve build script

--- ksql/build-ksql.sh
+++ ksql/build-ksql.sh
@@ -2,6 +2,7 @@
 
 SRC_DIR=$1
 DIST_DIR=$2
+DEPLOYMENT_ENV=${3}
 
 if [ -z "$SRC_DIR" ]
 then
@@ -18,6 +19,11 @@ fi
 if [ -d $DIST_DIR ]
 then
   ls -d $SRC_DIR/* | egrep -i '.*([0-9]{6}).*.ksql$' | xargs cat > $DIST_DIR/main.ksql
+  if [ -z "${DEPLOYMENT_ENV}" ]; then
+    echo -e "$(cat env.ksql)\n$(cat main.ksql)" > main.ksql
+  else
+    echo -e "$(cat env.$DEPLOYMENT_ENV.ksql)\n$(cat main.ksql)" > main.ksql
+  fi
 else
   echo "Error: Directory $DIST_DIR does not exists."
 fi
